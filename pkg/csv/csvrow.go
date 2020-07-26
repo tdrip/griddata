d47919068218c65d330872106c9959beb3a9c4bf
will.allen <will.allen@venafi.com>
2020-07-26 08:46:05 +0100

    updated csv row to use create cell

--- pkg/csv/csvrow.go
+++ pkg/csv/csvrow.go
@@ -14,12 +14,15 @@ type HeadedRowData struct {
 	gd.GDHeadedRowData
 }
 
+//CreateRowData
 func CreateRowData(row int, datain []string) *RowData {
 	rd := RowData{}
 
+	cells := []*gd.GDCell{}
 	//rd.SetIndex
 	for d := 0; d < len(datain); d++ {
-
+		cell := gd.CreateStringCell(gd.CreateGDPoint(row, d), datain[d])
+		cells := append(cells)
 	}
 
 	return &rd
