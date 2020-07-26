0274690ec6a4841aea72c64fdfa5e282764c00c1
will.allen <will.allen@venafi.com>
2020-07-26 08:32:57 +0100

    Using logger

--- pkg/csv/rowparser.go
+++ pkg/csv/rowparser.go
@@ -3,7 +3,6 @@ package csv
 import (
 	"errors"
 	"io"
-	"log"
 
 	gd "github.com/tdrip/griddata/pkg"
 	igrid "github.com/tdrip/griddata/pkg/interfaces"
@@ -24,26 +23,33 @@ func CreateCSVRowParser() *CSVRowParser {
 //Parse parse the data source
 func (rd *CSVRowParser) Parse(parent igrid.IParser, data igrid.IDataSource) error {
 
+	// convert the idatasource to what we expect which is a CSV File
 	csvdata := data.(*CSVFile)
+
+	// We need a GD Parser for the logging
 	gdp := parent.(*gd.GDParser)
 
 	if csvdata != nil {
+		row := 0
 		for {
-
 			record, err := csvdata.Reader.Read()
 			if err == io.EOF {
+				gdp.Logger.LogDebug("Parse", "End of file")
 				break
 			}
 			if err != nil {
-				log.Fatal(err)
+				gdp.Logger.LogErrorE("Parse", err)
+				break
+			} else {
+				gdp.Logger.LogInfo("Parse", record)
 			}
-			gdp.Logger.LogInfo("Parse", record)
+			row++
 		}
 
 		return nil
 
 	}
 
-	return errors.New("Data source was not of type CSV Files or was nil")
+	return errors.New("Data source provided was not of type CSV File or was nil")
 
 }
