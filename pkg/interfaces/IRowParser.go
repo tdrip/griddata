609f28b6a6a241dae5d6529f0aaa26752de101f4
will.allen <will.allen@venafi.com>
2020-07-29 21:30:08 +0100

    added new funtions

--- pkg/interfaces/IRowParser.go
+++ pkg/interfaces/IRowParser.go
@@ -1,10 +1,17 @@
 package igrid
 
+//IRowParsingOptions Represents the options for parsing a data row
 type IRowParsingOptions interface {
+	String() string
 }
 
 //IRowParser This interface parses a row in the grid
 type IRowParser interface {
+
+	// Options for parsing the row
+	GetOptions() IRowParsingOptions
+	SetOptions(options IRowParsingOptions)
+
 	// Parse the row
 	Parse(parent IParser, data IDataSource) error
 }
