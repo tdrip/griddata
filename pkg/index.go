adaa9a322982162e4571d13d0f7af3cb22f5cd66
will.allen <will.allen@venafi.com>
2020-07-29 21:30:16 +0100

    comments

--- pkg/index.go
+++ pkg/index.go
@@ -15,28 +15,34 @@ type Index struct {
 	RelatedIndexes []igrid.IIndex
 }
 
+//CreateIndex Creates a pointer to the Index struct
 func CreateIndex(location igrid.IPoint) *Index {
 	gdi := Index{}
 	gdi.Location = location
 	return &gdi
 }
 
+//GetPosition return the position of this Index
 func (gdi *Index) GetPosition() igrid.IPoint {
 	return gdi.Location
 }
 
+//SetPosition set the position of this Index
 func (gdi *Index) SetPosition(position igrid.IPoint) {
 	gdi.Location = position
 }
 
+//GetRelatedIndexes return indexes that are related to this one
 func (gdi *Index) GetRelatedIndexes() []igrid.IIndex {
 	return gdi.RelatedIndexes
 }
 
+//SetRelatedIndexes set indexes that are related to this one
 func (gdi *Index) SetRelatedIndexes(relatedi []igrid.IIndex) {
 	gdi.RelatedIndexes = relatedi
 }
 
+//AddRelatedIndex add an index that is related to this one
 func (gdi *Index) AddRelatedIndex(relatedi igrid.IIndex) {
 	rindices := gdi.RelatedIndexes
 	rindices = append(rindices, relatedi)
