780781ff9ff3d063baa532eb10d7369d53443f8f
tdrip <willbobaggins0@gmail.com>
2022-08-10 02:08:33 +0100

    more tests

--- pkg/csv/parser_test.go
+++ pkg/csv/parser_test.go
@@ -31,7 +31,7 @@ func TestCSV(t *testing.T) {
 
 }
 
-func TestCSV2(t *testing.T) {
+func TestCSVActions(t *testing.T) {
 
 	log := logr.New()
 
@@ -48,6 +48,40 @@ func TestCSV2(t *testing.T) {
 	csvtest := CSVRowAction{ID: "PrintAction"}
 
 	gdp := CreateFileParserWithAction(log, "../../testdata/noheader.csv", &csvtest)
+
+	err := gdp.Execute()
+
+	if err != nil {
+		t.Errorf("%s  ", err.Error())
+	}
+
+}
+
+func TestCSV3Passes(t *testing.T) {
+
+	log := logr.New()
+
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&logr.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(logr.TraceLevel)
+
+	csvtest := CSVRowAction{ID: "PrintAction"}
+
+	gdp := CreateFileParserWithAction(log, "../../testdata/noheader.csv", &csvtest)
+	rowprocessors := gdp.GetRowProcessors()
+
+	opts := rowprocessors[0].GetOptions()
+	rpo := opts.(*RowProcessorOptions)
+	rpo.TotalPasses = 3
+	rowprocessors[0].SetOptions(rpo)
+
+	gdp.SetRowProcessors(rowprocessors)
 
 	err := gdp.Execute()
 
